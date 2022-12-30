1317eeba6fd2ba4327f57fc786709d6553bffd57
robn8 <rob.natale.8@gmail.com>
2022-12-30 14:45:40 -0600

    initial commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen of music</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
---
+++ src/Screens/KeyScreen.js
@@ -0,0 +1,29 @@
+/* 
+
+const aMajor = {
+    name: 'A Major',
+    keySignature: 'F#',
+    chords: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+    scale: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#', 'A'],
+    relativeMinor: 'F# Minor'
+  };
+
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'mydatabase';
+
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+  if (err) throw err;
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  const collection = db.collection('documents');
+  collection.insertOne(aMajor, function(err, result) {
+    console.log("Inserted object into the collection");
+    client.close();
+  });
+});
+
+*/
