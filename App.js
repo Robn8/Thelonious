4815ed37951255bea460b1693e9cde1626aa1699
robn8 <rob.natale.8@gmail.com>
2023-01-01 19:55:53 -0600

    Added Theory bottom tab screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomeScreen from './src/Screens/HomeScreen';
-
+import theoryScreen from './src/Screens/TheoryScreen';
 
 const tabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
+  Theory: theoryScreen,
 });
 
 export default createAppContainer(tabNavigator);
--- src/Screens/TheoryScreen.js
+++ src/Screens/TheoryScreen.js
@@ -2,14 +2,16 @@ import { Text, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import React from 'react';
 
-const HomeScreen = () => {
+const TheoryScreen = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <Text>Home Screen</Text>
+            <Text>Now were learning!</Text>
+            <Text>What the key means:</Text>
+            <Text>What does the Dominant chord of the scale do:</Text>
         </SafeAreaView>
     )
 };
 
 const styles = StyleSheet.create({});
 
-export default HomeScreen;
+export default TheoryScreen;
