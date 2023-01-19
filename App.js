02560b941092459bb7ef19b6a767d0404b843059
Robert Natale <rob.natale.8@gmail.com>
2023-01-19 15:25:45 -0600

    Added Keys Screen and tab to bottom navigation.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomeScreen from './src/Screens/HomeScreen';
-import theoryScreen from './src/Screens/TheoryScreen';
+import TheoryScreen from './src/Screens/TheoryScreen';
+import KeyScreen from './src/Screens/KeyScreen';
 
 const tabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Theory: theoryScreen,
+  Theory: {
+    screen: TheoryScreen,
+  },
+  Home: {
+    screen: HomeScreen,
+  },
+  Keys: {
+    screen: KeyScreen,
+  },
 });
 
 export default createAppContainer(tabNavigator);
--- src/Screens/KeyScreen.js
+++ src/Screens/KeyScreen.js
@@ -1,29 +1,15 @@
-/* 
+import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import React from 'react';
 
-const aMajor = {
-    name: 'A Major',
-    keySignature: 'F#',
-    chords: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
-    scale: ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#', 'A'],
-    relativeMinor: 'F# Minor'
-  };
+const KeyScreen = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>Key Screen</Text>
+        </SafeAreaView>
+    )
+};
 
-const MongoClient = require('mongodb').MongoClient;
+const styles = StyleSheet.create({});
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'mydatabase';
-
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  if (err) throw err;
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  const collection = db.collection('documents');
-  collection.insertOne(aMajor, function(err, result) {
-    console.log("Inserted object into the collection");
-    client.close();
-  });
-});
-
-*/
+export default KeyScreen;
