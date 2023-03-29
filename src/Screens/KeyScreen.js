d7cf873d5e503771eb3bf012932934434da3381b
robn8 <rob.natale.8@gmail.com>
2023-03-29 17:01:05 -0500

    Initial Key Screen set up

--- src/Screens/KeyScreen.js
+++ src/Screens/KeyScreen.js
@@ -1,15 +1,36 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import React from 'react';
 
 const KeyScreen = () => {
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <Text>Key Screen</Text>
-        </SafeAreaView>
+            <SafeAreaView style={styles.container}>
+                <Text style={styles.header}>Select A Key To Find Out More!</Text>
+                <Text style={styles.letterStyle}>A</Text>
+                <Text style={styles.letterStyle}>B</Text>
+                <Text style={styles.letterStyle}>C</Text>
+                <Text style={styles.letterStyle}>D</Text>
+                <Text style={styles.letterStyle}>E</Text>
+                <Text style={styles.letterStyle}>F</Text>
+                <Text style={styles.letterStyle}>G</Text>
+            </SafeAreaView>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    letterStyle: {
+        fontSize: 64,
+        marginBottom: 15
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    header: {
+        marginBottom: 15,
+        fontSize: 24,
+    }
+});
 
 export default KeyScreen;
